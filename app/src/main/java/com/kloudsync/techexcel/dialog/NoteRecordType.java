31ffd057dcc36c506301b11ecd3abe0f044bd5ea
xfwang <1599528112@qq.com>
2020-03-04 16:41:24 +0800

    update

--- app/src/main/java/com/kloudsync/techexcel/dialog/NoteRecordType.java
+++ app/src/main/java/com/kloudsync/techexcel/dialog/NoteRecordType.java
@@ -9,7 +9,7 @@ public enum  NoteRecordType {
     DRAW_LINE(304), //划线(pengfei)
     CLOSE_POPUP_NOTE(305),//关闭浮窗笔记(前端)
     CLOSE_HOMEPAGE_NOTE(306),//关闭主界面笔记(前端)
-    DISPLAY_HOME_POPUP(307),//笔记从主界面显示到浮窗(前端)
+    DISPLAY_HOMEPAGE_POPUP(307),//笔记从主界面显示到浮窗(前端)
     DISPALY_HOMEPAGE(308);//直接显示笔记到主界面: (开始录的时候已经有笔记在主界面,前端记,后来有第一个笔记pengfei记)
 
     private final int actiontype;
