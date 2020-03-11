36cdf6a642f30ba84ceb7e5f9b4583c52eb1a443
xfwang <1599528112@qq.com>
2020-03-11 10:08:14 +0800

    update

--- app/src/main/java/com/kloudsync/techexcel/dialog/RecordNoteActionManager.java
+++ app/src/main/java/com/kloudsync/techexcel/dialog/RecordNoteActionManager.java
@@ -1,7 +1,9 @@
 package com.kloudsync.techexcel.dialog;
 
 import android.content.Context;
+import android.util.Log;
 
+import com.kloudsync.techexcel.bean.MeetingConfig;
 import com.ub.techexcel.tools.Tools;
 
 import org.json.JSONArray;
@@ -13,12 +15,10 @@ public class RecordNoteActionManager {
     static volatile RecordNoteActionManager instance;
     private Context mContext;
     private SoundtrackRecordManager soundtrackRecordManager;
-
     public RecordNoteActionManager(Context context) {
         this.mContext = context;
         soundtrackRecordManager=SoundtrackRecordManager.getManager(mContext);
     }
-
     public static RecordNoteActionManager getManager(Context context) {
         if (instance == null) {
             synchronized (RecordNoteActionManager.class) {
@@ -30,11 +30,11 @@ public class RecordNoteActionManager {
         return instance;
     }
 
-
     /**
-     * 304 画线
+     * 5.画线(前端)
+     * {actionType:304,time:1,page:1,data:{duration:11,strokeId:"3243-2342-24-4"}}
      * @param noteId  笔记id
-     * @param noteData 笔记单条数据
+     * @param noteData 笔记单条数据  noteData
      */
     public void sendDrawActions(long noteId, String noteData) {
 //        {actionType:304,time:1,page:1,data:{duration:11,strokeId:"3243-2342-24-4"}}
@@ -77,23 +77,166 @@ public class RecordNoteActionManager {
 
 
     /**
-     * 直接显示笔记到主界面  308
+     * 1. 显示笔记到浮窗: (前端)
+     * {actionType:300,time:1,page:1,data:{id:123,lastStrokeId:"1-2-3"}}
      * @param noteId
-     * @param linejson
+     * @param paintData
      */
-    public  void sendDisplayHomePageActions(long noteId, JSONObject linejson){
+    public  void sendDisplayPopupActions(long noteId, JSONObject paintData){
+        //  {
+        //	"PageTokenBackup": "002ff42a-6f76-4d67-bfd5-c216116b9b57",
+        //	"PageToken": "3a986f8f-0d31-4241-8ac0-a85a2e75ebc6",
+        //	"PaintData": {
+        //		"address": "45.0.0.2",
+        //		"lines": [{
+        //					"id": "e4a3276e-f871-4736-a608-ea39a1a7c4ae",
+        //					"pen": "1CA127B9-0E81-A668-D172-72F4AC8815B8",
+        //					"user": 225014321,
+        //					"points": [
+        //						[758, 5735, 740, 1578967168],
 //      {actionType:308,time:1,page:1,data:{id:123,lastStrokeId:"1-2-3"}}
         String strokeId="";
         try {
-            JSONArray linesjson=linejson.getJSONArray("lines");
+            JSONArray linesjson=paintData.getJSONArray("lines");
             if(linesjson.length()>0){
                 JSONObject linedata=linesjson.getJSONObject(linesjson.length()-1);
                 strokeId=linedata.getString("id");  //拿最后一条动作的id
             }
-            JSONObject jsonObject=new JSONObject();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject= null;
+        try {
+            jsonObject = new JSONObject();
             jsonObject.put("id",noteId);
             jsonObject.put("lastStrokeId",strokeId);
-            soundtrackRecordManager.recordNoteAction(NoteRecordType.DISPALY_HOMEPAGE,jsonObject);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        soundtrackRecordManager.recordNoteAction(NoteRecordType.DISPLAY_POPUP,jsonObject);
+    }
+
+    /**
+     *
+     * 9. 直接显示笔记到主界面: (前端,meeting:server)
+     * {actionType:308,time:1,page:1,data:{id:123,lastStrokeId:"1-2-3"}}
+     * @param noteId
+     * @param paintData
+     */
+    public  void sendDisplayHomePageActions(long noteId, JSONObject paintData){
+        //  {
+        //	"PageTokenBackup": "002ff42a-6f76-4d67-bfd5-c216116b9b57",
+        //	"PageToken": "3a986f8f-0d31-4241-8ac0-a85a2e75ebc6",
+        //	"PaintData": {
+        //		"address": "45.0.0.2",
+        //		"lines": [{
+        //					"id": "e4a3276e-f871-4736-a608-ea39a1a7c4ae",
+        //					"pen": "1CA127B9-0E81-A668-D172-72F4AC8815B8",
+        //					"user": 225014321,
+        //					"points": [
+        //						[758, 5735, 740, 1578967168],
+//      {actionType:308,time:1,page:1,data:{id:123,lastStrokeId:"1-2-3"}}
+        String strokeId="";
+        try {
+            JSONArray linesjson=paintData.getJSONArray("lines");
+            if(linesjson.length()>0){
+                JSONObject linedata=linesjson.getJSONObject(linesjson.length()-1);
+                strokeId=linedata.getString("id");  //拿最后一条动作的id
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject= null;
+        try {
+            jsonObject = new JSONObject();
+            jsonObject.put("id",noteId);
+            jsonObject.put("lastStrokeId",strokeId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        soundtrackRecordManager.recordNoteAction(NoteRecordType.DISPALY_HOMEPAGE,jsonObject);
+    }
+
+
+    /**
+     * 2.笔记从浮窗显示到主界面(前端)
+     * {actionType:301,time:1,page:1,data:{id:343,lastStrokeId:"1-2-3"}}
+     */
+    public  void sendDisplayPopupHomepageActions(long noteId,JSONObject paintData){
+        soundtrackRecordManager.recordNoteAction(NoteRecordType.DISPLAY_POPUP_HOMEPAGE,getHomepagePopupLineData(noteId,paintData));
+    }
+
+    /**
+     * 8.笔记从主界面显示到浮窗(前端)
+     * {actionType:307,time:1,page:1,data:{id:343,lastStrokeId:"1-2-3"}}
+     */
+    public void sendDisplayHomepagePopupActions(long noteId,JSONObject paintData){
+        soundtrackRecordManager.recordNoteAction(NoteRecordType.DISPLAY_HOMEPAGE_POPUP,getHomepagePopupLineData(noteId,paintData));
+    }
+
+    /**
+     * 浮窗与主界面切换要录制的数据
+     * @param noteId
+     * @param paintData
+     * @return
+     */
+    private JSONObject getHomepagePopupLineData(long noteId,JSONObject paintData){
+        String strokeId="";
+        try {
+            JSONArray linesjson=paintData.getJSONArray("lines");
+            if(linesjson.length()>0){
+                JSONObject linedata=linesjson.getJSONObject(linesjson.length()-1);
+                strokeId=linedata.getString("id");  //拿最后一条动作的id
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JSONObject jsonObject= null;
+        try {
+            jsonObject = new JSONObject();
+            jsonObject.put("id",noteId);
+            jsonObject.put("lastStrokeId",strokeId);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
+
+    /**
+     * 6.关闭浮窗笔记(前端)
+     * {actionType:305,time:1,page:1,data:{id:123}}
+     */
+    public void sendClosePopupActons(long noteId){
+
+        try {
+            JSONObject jsonObject=new JSONObject();
+            jsonObject.put("id",noteId);
+            soundtrackRecordManager.recordNoteAction(NoteRecordType.CLOSE_POPUP_NOTE,jsonObject);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * 7.关闭主界面笔记(前端)
+     * {actionType:306,time:1,page:1,data:{id:123,docId:345,pageNo:1}}
+     */
+    public void sendCloseHomePageActon(long noteId, boolean isMeetingRecord, MeetingConfig meetingConfig){
+        // {actionType:306,time:1,page:1,data:{id:123,docId:345,pageNo:1}}
+        // id: noteid
+        // docId:关闭笔记后显示的文档id(会议录制增加)
+        // pageNo:显示的文档页码(会议录制增加)
+        try {
+            JSONObject jsonObject=new JSONObject();
+            jsonObject.put("id",noteId);
+            if(isMeetingRecord){
+                jsonObject.put("docId",meetingConfig.getDocument().getItemID());
+                jsonObject.put("pageNo",meetingConfig.getPageNumber());
+            }
+            soundtrackRecordManager.recordNoteAction(NoteRecordType.CLOSE_HOMEPAGE_NOTE,jsonObject);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -101,18 +244,10 @@ public class RecordNoteActionManager {
 
 
     /**
-     * 关闭主界面笔记 306
-     */
-    private void sendCloseHomePageActon(){
-
-
-    }
-
-
-    /**
      * 笔记换页  302
      */
     private void sendChangepageActions(){
+
 
     }
 
