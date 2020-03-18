c2097ec375b7acfe37c99732ba9192e3e16da214
xfwang <1599528112@qq.com>
2020-03-18 16:13:17 +0800

    update

--- app/src/main/java/com/kloudsync/techexcel/ui/DocAndMeetingActivity.java
+++ app/src/main/java/com/kloudsync/techexcel/ui/DocAndMeetingActivity.java
@@ -919,7 +919,7 @@ public class DocAndMeetingActivity extends BaseDocAndMeetingActivity implements 
         if (meetingConfig.getType() == MeetingType.MEETING) {
             noteUsersLayout.setVisibility(View.GONE);
         } else {
-            noteUsersLayout.setVisibility(View.VISIBLE);
+            noteUsersLayout.setVisibility(View.GONE);
         }
         Log.e("followShowNote", "noteid:" + noteId);
         hideEnterLoading();
@@ -3194,7 +3194,13 @@ public class DocAndMeetingActivity extends BaseDocAndMeetingActivity implements 
             int noteId = retData.getInt("noteId");
             int status = retData.getInt("status");
             if (status == 1) { //打开浮窗
-                showNoteFloatingDialog(noteId);
+                if (noteLayout.getVisibility() == View.VISIBLE) {
+                    if (noteWeb != null) {
+                       followShowNote(noteId);
+                    }
+                }else{
+                    showNoteFloatingDialog(noteId);
+                }
             } else if (status == 0) {  //关闭浮窗 或者 主界面
                 if (noteLayout.getVisibility() == View.VISIBLE) {
                     if (noteWeb != null) {
