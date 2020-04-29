9afb5ddb98359fa5b3a95e63103eac6f30b38ea7
520hhy <594202hhy>
2020-04-29 23:46:13 +0800

    commit

--- app/src/main/java/com/ub/service/activity/NewMeetingActivity.java
+++ app/src/main/java/com/ub/service/activity/NewMeetingActivity.java
@@ -110,7 +110,7 @@ public class NewMeetingActivity extends Activity implements View.OnClickListener
         initView();
         if(bean!=null){
             submit.setEnabled(true);
-            as_rl_contact.setEnabled(true);
+            as_rl_contact.setEnabled(false);
             meetingname.setText(bean.getName());
             if (!TextUtil.isEmpty(bean.getPlanedStartDate())) {
                 SimpleDateFormat formatter = new SimpleDateFormat("HH:mm");
@@ -173,7 +173,7 @@ public class NewMeetingActivity extends Activity implements View.OnClickListener
             }
         }else {
             submit.setEnabled(false);
-            as_rl_contact.setEnabled(false);
+            as_rl_contact.setEnabled(true);
         }
     }
 
